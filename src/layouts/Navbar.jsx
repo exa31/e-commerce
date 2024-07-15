f7f4573f2ddf38c2475e0de6db90852530c22a56
exa31 <bloodsuker18@gmail.com>
2024-07-16 03:00:42 +0700

    add responsive navbar

--- src/layouts/Navbar.jsx
+++ src/layouts/Navbar.jsx
@@ -3,6 +3,8 @@ import { useSelector, useDispatch } from "react-redux";
 import Logout from "../pages/Logout";
 import { useState } from "react";
 import { clearAll, logout } from "../redux/defaultSlice";
+import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 
 export default function Navbar() {
@@ -28,6 +30,8 @@ export default function Navbar() {
         setIsLogout(!isLogout)
     }
 
+    const [showNav, setShowNav] = useState(false)
+
     const [isLogout, setIsLogout] = useState(false)
 
     return (
@@ -36,7 +40,11 @@ export default function Navbar() {
                 <div>
                     <h1 className="text-white text-4xl">Exa</h1>
                 </div>
-                <div>
+                <div className="md:hidden">
+                    <FontAwesomeIcon onClick={() => setShowNav(!showNav)} className={showNav ? "md:hidden hidden text-white" : "md:hidden text-white"} icon={faBars} />
+                    <FontAwesomeIcon onClick={() => setShowNav(!showNav)} className={showNav ? "md:hidden text-white" : "md:hidden hidden text-white"} icon={faXmark} />
+                </div>
+                <div className={showNav ? "flex duration-200 md:block flex-col bg-black w-6/12 md:w-auto h-full md:h-auto fixed md:static top-14 right-0" : "flex md:block flex-col bg-black w-0 md:w-max h-full duration-200 md:h-auto fixed md:static top-14 right-0"}>
                     <NavLink className={({ isActive, isPending }) =>
                         isActive
                             ? "active m-4 duration-200 text-white hover:cursor-default"
@@ -71,8 +79,17 @@ export default function Navbar() {
                                 ? "active m-4 duration-200 text-white hover:cursor-default"
                                 : "m-4 duration-200 text-white hover:opacity-70"
                     } to={user ? "/history" : "/login"}>History</NavLink>
+                    <div className="md:hidden">
+                        {user ?
+                            <button onClick={handleClick} to="/logout" className="py-2 px-6 m-2 hover:bg-slate-800 bg-black duration-200 transition-all text-white border-2">Logout</button>
+                            :
+                            <>
+                                <NavLink className="py-2 px-6 m-2 hover:bg-slate-800 bg-black duration-200 transition-all text-white border-2" to="/login">Login</NavLink>
+                            </>
+                        }
+                    </div>
                 </div>
-                <div>
+                <div className="md:block hidden">
                     {user ?
                         <button onClick={handleClick} to="/logout" className="py-2 px-6 m-2 hover:bg-slate-800 bg-black duration-200 transition-all text-white border-2">Logout</button>
                         :
